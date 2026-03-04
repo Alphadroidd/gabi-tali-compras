4cccc4e6a0abfb4bfabf1b42a30da279734adab0
Alphadroidd <68525144+Alphadroidd@users.noreply.github.com>
2026-03-04 18:50:02 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,23 @@ const CACHE_NAME = "gabi-tali-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
